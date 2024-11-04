b467d5a164a6aa66d2e495b85a9ea09d29764dc1
aniketmore-pixel <aniketmore.personal@gmail.com>
2024-11-04 21:39:01 +0530

    Changed localhost

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/register",
+      "https://ctserver-qth5.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://ctserver-qth5.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
